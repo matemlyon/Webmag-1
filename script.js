c7b0e442f2668707b749d8deb64d5499c6857bec
matemlyon <mathilde.saker@latoile.em-lyon.com>
2024-12-06 12:30:48 +0100

    Update

--- script.js
+++ script.js
@@ -1,19 +1,113 @@
 function getData() {
-   fetch('data.json')
-     .then((response) => {
-       if (!response.ok) {
-         throw new Error('Network response was not ok');
-       }
-       return response.json();
-     })
-     .then((data) => {
-       // Traitez les données comme vous le souhaitez
-       console.log('Données récupérées du fichier JSON :', data);
-       /// ON ECRIT LE CODE ICI ! 
-      
-       /// FIN DU CODE
-     })
-     .catch((error) => console.error('Erreur lors de la lecture des données :', error));
- }
- 
- getData();
+  fetch('data.json')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log('Données récupérées du fichier JSON :', data);
+      /// ON ECRIT LE CODE ICI !
+     let journal= data.journal;
+     console.log(journal);
+     let nomJournal = journal.nomJournal;
+     console.log(nomJournal);
+     let phraseAccroche = journal.phraseAccroche;
+     console.log(phraseAccroche);
+     let texteAppelAction= journal.texteAppelAction;
+     console.log(texteAppelAction);
+     let articlePrincipal= journal.articlePrincipal;
+     console.log(articlePrincipal);
+     let titre= articlePrincipal.titre;
+     console.log(titre);
+     let description= articlePrincipal.description;
+     console.log(description);
+     let date= articlePrincipal.date;
+     console.log(date);
+     let theme= articlePrincipal.theme;
+     console.log(theme);
+     let image= articlePrincipal.image;
+     console.log(image);
+     let articlePrincipalContainer= document.getElementById('article-principal');
+      afficherArticle(articlePrincipal, articlePrincipalContainer);
+      let themesContainer = document.getElementById('themes-section');
+      afficherTheme(journal.themes, themesContainer);
+      let articlesContainer = document.querySelector('.articles-containers');
+      journal.articles.forEach((article) => {
+        afficherArticle(article, articlesContainer);
+      });
+      let auteursContainer = document.querySelector('.auteurs-container');
+      journal.auteurs.forEach((auteur) => {
+        afficherAuteur(auteur, auteursContainer);
+      });
+    })
+      /// FIN DU CODE
+    .catch((error) => console.error('Erreur lors de la lecture des données :', error));
+  }
+getData();
+///ON écrit les fonctions ici
+function afficherArticlePrincipal(article, container) {
+  let titre = article.titre;
+  let date = article.date;
+  let theme = article.theme;
+  let image = article.image;
+  let description = article.description;
+  let element = `
+    <div class="article-principal">
+      <img src="${image}" alt="${titre}">
+      <h1>${titre}</h1>
+      <h2>${theme} / ${date}</h2>
+      <p>${description}</p>
+      <button class="article-button">
+        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=ton-adresse@email.com" target="_blank" rel="noopener noreferrer">Lire l'article</a>
+      </button>
+    </div>`;
+  container.innerHTML = element;
+}
+function afficherTheme(themes, container){
+  themes.forEach((theme) => {
+   let nom = theme.nom;
+   let description = theme.description;
+   let element = `
+   <div class="theme">
+     <h3>${nom}</h3>
+     <p>${description}</p>
+   </div>`;
+  container.insertAdjacentHTML('beforeend', element);
+  });
+  }
+function afficherArticle(article, container){
+ let titre = article.titre;
+ let date = article.date;
+ let theme= article.theme;
+ let image = article.image;
+ let element = `
+  <div class="article-principal">
+  <img src="${image}" alt="${titre}">
+        <h1>${titre}</h1>
+        <h2>${theme} / ${date}</h2>
+       <p>${article.description}</p>
+        <button class="article-button">
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=ton-adresse@email.com" target="_blank" rel="noopener noreferrer">Lire l'article</a>
+        </button>
+    </div>`;
+    container.insertAdjacentHTML('beforeend', element);
+}
+function afficherAuteur(auteur, container){
+ let prenom = auteur.prenom;
+ let typeExperience = auteur.typeExperience;
+ let presentation = auteur.presentation;
+let element = `
+    <div class="auteur">
+      <div class="author">
+        <img src="images/default-avatar.png" alt="Auteur ${prenom}">
+        <div>
+          <h4>${prenom}</h4>
+          <p>${typeExperience}</p>
+          <p>${presentation}</p>
+        </div>
+      </div>
+    </div>`;
+    container.insertAdjacentHTML('beforeend', element);
+  }
